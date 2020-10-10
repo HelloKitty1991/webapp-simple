ea588443acbfe053ec0e5b783df30fff554433bc
penganhua <penganhua@klicen.com>
2020-10-10 17:15:48 +0800

    remove the keywords

--- src/demo1/index.js
+++ src/demo1/index.js
@@ -1,3 +1,1 @@
-import request from '@klicen/utils/lib/request';
-const req = request();
-import './index.css';
+
--- src/demo2/index.js
+++
@@ -1,4 +0,0 @@
-import request from '@klicen/utils/lib/request';
-const req = request();
-console.log('demo2');
-console.log('33333333');
